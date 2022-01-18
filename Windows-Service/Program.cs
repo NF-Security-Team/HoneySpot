0f3c0b033c5213b2c043f6284bd42d4a94b15cfc
SOC-LaneWan <nicolas.fasolo@hotmail.it>
2022-01-18 16:53:02 +0100

    Full Commit Windows Service Base
    - Windows Service Template
    - TODO --> Tutto il resto

---
+++ Windows-Service/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.ServiceProcess;
+using NLog;
+using Exceptionless;
+
+namespace HoneySpotService
+{
+    static class Program
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private static HoneySpotService _service;
+
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        static void Main(string[] args)
+        {
+            ExceptionlessClient.Default.CreateEvent();
+            AppSettings.Init();
+
+            // self service installer/uninstaller
+            if (args != null && args.Length == 1
+                && (args[0][0] == '-' || args[0][0] == '/'))
+            {
+                switch (args[0].Substring(1).ToLower())
+                {
+                    case "install":
+                    case "i":
+                        if (!ServiceInstallerUtility.InstallMe())
+                            Logger.Fatal("Failed to install service");
+                        break;
+                    case "uninstall":
+                    case "u":
+                        if (!ServiceInstallerUtility.UninstallMe())
+                            Logger.Fatal("Failed to uninstall service");
+                        break;
+                    default:
+                        Logger.Error("Unrecognized parameters (allowed: /install and /uninstall, shorten /i and /u)");
+                        break;
+                }
+                Environment.Exit(0);
+            }
+
+            _service = new HoneySpotService();
+            var servicesToRun = new ServiceBase[] { _service };
+
+            // console mode
+            if (Environment.UserInteractive)
+            {
+                // register console close event
+                _consoleHandler = ConsoleEventHandler;
+                SetConsoleCtrlHandler(_consoleHandler, true);
+
+                Console.Title = AppDomain.CurrentDomain.FriendlyName;
+
+                Logger.Debug("Running in console mode");
+                _service.Start();
+
+                Console.WriteLine("Press any key to stop the service...");
+                Console.Read();
+
+                _service.Stop();
+            }
+            // service mode
+            else
+            {
+                Logger.Debug("Running in service mode");
+                ServiceBase.Run(servicesToRun);
+            }
+        }
+
+
+
+
+        #region Page Event Setup
+        enum ConsoleCtrlHandlerCode : uint
+        {
+// ReSharper disable InconsistentNaming
+            CTRL_C_EVENT = 0,
+            CTRL_BREAK_EVENT = 1,
+            CTRL_CLOSE_EVENT = 2,
+            CTRL_LOGOFF_EVENT = 5,
+            CTRL_SHUTDOWN_EVENT = 6
+// ReSharper restore InconsistentNaming
+        }
+        delegate bool ConsoleCtrlHandlerDelegate(ConsoleCtrlHandlerCode eventCode);
+        [DllImport("kernel32.dll")]
+        static extern bool SetConsoleCtrlHandler(ConsoleCtrlHandlerDelegate handlerProc, bool add);
+        static ConsoleCtrlHandlerDelegate _consoleHandler;
+        #endregion
+
+        #region Page Events
+        static bool ConsoleEventHandler(ConsoleCtrlHandlerCode eventCode)
+        {
+            // Handle close event here...
+            switch (eventCode)
+            {
+                case ConsoleCtrlHandlerCode.CTRL_C_EVENT:
+                case ConsoleCtrlHandlerCode.CTRL_CLOSE_EVENT:
+                case ConsoleCtrlHandlerCode.CTRL_BREAK_EVENT:
+                case ConsoleCtrlHandlerCode.CTRL_LOGOFF_EVENT:
+                case ConsoleCtrlHandlerCode.CTRL_SHUTDOWN_EVENT:
+
+                    _service.Stop();
+
+                    Environment.Exit(0);
+                    break;
+            }
+
+            return (false);
+        }
+        #endregion
+    }
+}
---
+++ Windows-Service/TestService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading;
+using NLog;
+
+namespace HoneySpotService
+{
+    /// <summary>
+    /// Example service, rename and modify it to suit your needs
+    /// </summary>
+    public class TestService
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly Thread _thread;
+        public TestService()
+        {
+            _thread = new Thread(DoWork);
+        }
+
+        public void Start()
+        {
+            _thread.Start();
+        }
+        public void Stop()
+        {
+            _thread.Abort();
+        }
+
+
+        private void DoWork()
+        {
+            var i = AppSettings.StartFrom;
+            while (true)
+            {
+                Logger.Debug("Ping " + i);
+                i++;
+
+                Thread.Sleep(1000);
+            }
+// ReSharper disable once FunctionNeverReturns
+        }
+
+
+    }
+}
