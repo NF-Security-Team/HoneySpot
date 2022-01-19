b03fcc8c2b93f01836f1c26bdb167fdb0cff9c2f
PasinAndrea97 <44051481+PasinAndrea97@users.noreply.github.com>
2022-01-19 21:38:54 +0100

    HoneySpot - Windows Service folder renamed

---
+++ HoneySpotService/HoneySpotService.cs
@@ -0,0 +1,41 @@
+﻿using System.ServiceProcess;
+using NLog;
+
+namespace HoneySpotService
+{
+    public partial class HoneySpotService : ServiceBase
+    {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        private readonly HoneySpotListenerSRV s;
+        public HoneySpotService()
+        {
+            InitializeComponent();
+            s = new HoneySpotListenerSRV();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            Logger.Info("Start event");
+            s.Start();
+        }
+
+        protected override void OnStop()
+        {
+            Logger.Info("Stop event");
+            s.Stop();
+        }
+
+        protected override void OnShutdown()
+        {
+            Logger.Info("Windows is going shutdown");
+            Stop();
+        }
+
+
+        public void Start()
+        {
+            OnStart(null);
+        }
+    }
+}
---
+++ HoneySpotService/README.md
@@ -0,0 +1,12 @@
+
+HoneySpotService
+========================
+**Built using windows service template from HarpyWar https://github.com/HarpyWar/windows-service-template
+
+## Features
+
+* Run your program in both console or service mode
+* Self installer/uninstaller using command line args `/install` and `/uninstall` (shorten `/i` and `/u`)
+* [Formo](https://github.com/ChrisMissal/Formo) for easy reading `App.settings` properties
+* [NLog](https://github.com/NLog/NLog) for logging
+* [Exceptionless](https://github.com/exceptionless/Exceptionless.Net) for automatic logging of  unhandled exceptions
